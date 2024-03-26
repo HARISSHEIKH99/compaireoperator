cad9fadd87a51aed8f1062ac4ea52f11db5ad287
HARISSHEIKH99 <161943120+HARISSHEIKH99@users.noreply.github.com>
2024-03-26 03:32:59 -0700

    Add files via upload

---
+++ conditions.ts
@@ -0,0 +1,9 @@
+let month = "Ramadan";
+if(month == "Ramadan"){
+    console.log("water Bottel Is Not Allow");
+}
+let age =  17;
+if (age > 17){
+    console.log("ID Card Bana Sakty Ho")
+} else {console.log("your Are Under Age")
+};
---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+let afterRamadan = "Eid";
+console.log(afterRamadan == "Eid");
+let AC = "coling";
+console.log(AC == "coling");
+let favoriteDish = "biryani";
+console.log(favoriteDish != "korma");
+let Age = 25;
+console.log(Age > 25);
+console.log(Age >= 25);
+let months = 12;
+console.log(months < 12);
+console.log(months <= 12);
+//modulus
+let num1 = 5; //denominater
+let num2 = 17; //nominater
+console.log(num1 % num2); //rimender 5
